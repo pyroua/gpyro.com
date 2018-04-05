64dc1cda74d48ff285407ee3b56bf48263d44d33
Maksim <vampire.mr@gmail.com>
2018-04-05 23:27:02 +0300

    dev pass

--- web/index.php
+++ web/index.php
@@ -1,5 +1,24 @@
 <?php
 
+if(isset($_SERVER['HTTP_HOST']) && substr($_SERVER['HTTP_HOST'], 0, 3) == 'dev') {
+    $user = 'dev';
+    $pass = 'wJ*$Hu?@f6f5p$ph';
+
+    if (!isset($_SERVER['PHP_AUTH_USER'])) {
+        header('WWW-Authenticate: Basic realm="http://test"');
+        header('HTTP/1.0 401 Unauthorized');
+        echo 'Пользователь нажал кнопку Cancel admin';
+        exit();
+    } else {
+        if (($_SERVER['PHP_AUTH_USER'] != $user) || ($_SERVER['PHP_AUTH_PW'] != $pass)) {
+            header('WWW-Authenticate: Basic realm="http://test"');
+            header('HTTP/1.0 401 Unauthorized');
+            echo 'Пользователь нажал кнопку Cancel';
+            exit();
+        }
+    }
+}
+
 // comment out the following two lines when deployed to production
 defined('YII_DEBUG') or define('YII_DEBUG', true);
 defined('YII_ENV') or define('YII_ENV', 'dev');
