bdb859dc514c9df68e5c63f5d992653f6958d758
artygan <artygan@gmail.com>
2018-04-14 14:56:45 +0300

    DB charset fix

---
+++ console/migrations/m180414_115235_charset_fix.php
@@ -0,0 +1,26 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Class m180414_115235_charset_fix
+ */
+class m180414_115235_charset_fix extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->execute("ALTER DATABASE pyro CHARACTER SET utf8 COLLATE utf8_unicode_ci");
+        $this->execute("ALTER TABLE categories CONVERT TO CHARACTER SET utf8 COLLATE utf8_unicode_ci");
+        $this->execute("ALTER TABLE measures CONVERT TO CHARACTER SET utf8 COLLATE utf8_unicode_ci");
+        $this->execute("ALTER TABLE item_options CONVERT TO CHARACTER SET utf8 COLLATE utf8_unicode_ci");
+        $this->execute("ALTER TABLE item_option_categories CONVERT TO CHARACTER SET utf8 COLLATE utf8_unicode_ci");
+        $this->execute("ALTER TABLE item_option_values CONVERT TO CHARACTER SET utf8 COLLATE utf8_unicode_ci");
+        $this->execute("ALTER TABLE items CONVERT TO CHARACTER SET utf8 COLLATE utf8_unicode_ci");
+        $this->execute("ALTER TABLE migration CONVERT TO CHARACTER SET utf8 COLLATE utf8_unicode_ci;");
+    }
+
+
+}
