3c3f189b8e6206618d2de68900b83d380b8ba05b
artygan <artygan@gmail.com>
2018-04-17 15:02:05 +0300

    fix config

--- backend/config/params.php
+++ backend/config/params.php
@@ -1,11 +1,5 @@
 <?php
 return [
-    'adminEmail' => 'admin@example.com',
-    'uploadDirs' => [
-        'images' => [
-            'items' => '/var/www/gpyro.com/backend/web/img/items'
-        ]
-    ],
     'webDirs' => [
         'images' => [
             'items' => '/img/items'
