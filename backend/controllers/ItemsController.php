21ac3f0b36bb5cc16a0497bb5432602debcf3886
artygan <artygan@gmail.com>
2018-04-17 14:36:16 +0300

    fix rights

--- backend/controllers/ItemsController.php
+++ backend/controllers/ItemsController.php
@@ -131,7 +131,7 @@ class ItemsController extends BaseController
                             // save image
                             $imagesPath = $model->getImagesPath();
                             if (!is_dir($imagesPath)) {
-                                mkdir($imagesPath, 644, true);
+                                mkdir($imagesPath, 775, true);
                             }
 
                             $imageName = $formModel->getFileName();
